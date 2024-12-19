4da2187db1cc4e665830101fc611ab760f081a8c
joontube <iglu123@naver.com>
2024-12-19 15:49:16 +0900

    INIT: 프로젝트 생성

---
+++ src/main/java/com/example/imgdownload/ImgDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imgdownload/ImgDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imgdownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
